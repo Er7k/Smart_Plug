50b9115073a1a46c71d7b14691a6ab42e4c1e9cf
MarkSaltoV2 <markandersjoseph@gmail.com>
2025-03-04 22:03:59 +0100

    Lagt till meddelande för vad jag ändrat. Har bland annat lagt till en buffer för lagring av datan för de olika värdena för alla appliances

--- src/ApplianceGUI.java
+++ src/ApplianceGUI.java
@@ -1,21 +1,23 @@
+import se.mau.DA343A.VT25.projekt.Buffer;
+import se.mau.DA343A.VT25.projekt.ThreadHelp;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-
 public class ApplianceGUI implements ChangeListener {
 
     private final String title;
     private JSlider slider;
     private JLabel label;
-    private final double maxWATT;
-    private final double minWATT;
+    private Buffer<Double> buffer; // lagrar datan för värdena
+    private int maxPower; // alla Appliances har olika maxpower
 
-    public ApplianceGUI(String title, double maxWATT, double minWATT) {
+    public ApplianceGUI(String title, int maxPower) {
         this.title = title;
-        this.maxWATT = maxWATT;
-        this.minWATT = minWATT;
+        this.buffer = new Buffer<>();
+        this.maxPower = maxPower;
         createAndShowApplianceGUI();
     }
 
@@ -25,20 +27,20 @@ public class ApplianceGUI implements ChangeListener {
         frame.setSize(200, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setLocation(1100,0);
+        frame.setLocation(1100, 0);
 
-        slider = new JSlider(JSlider.VERTICAL, (int)minWATT, (int)maxWATT, 0);
+        slider = new JSlider(JSlider.VERTICAL, 0, maxPower, 0);
         slider.setPreferredSize(new Dimension(80, 400));
         slider.setPaintTrack(true);
         slider.setMajorTickSpacing(100);
         slider.setPaintLabels(true);
         slider.setPaintTicks(true);
         slider.setMinorTickSpacing(100);
-        slider.setBorder(new EmptyBorder(0,0,20,0));
+        slider.setBorder(new EmptyBorder(0, 0, 20, 0));
         slider.addChangeListener(this);
 
         label = new JLabel(("<html><div align='center'>" + title + "<br>(W) usage: <font color = 'blue'>" + slider.getValue() + "</div></html>"), SwingConstants.CENTER);
-        label.setBorder(new EmptyBorder(10,0,0,0));
+        label.setBorder(new EmptyBorder(10, 0, 0, 0));
 
         frame.add(slider, BorderLayout.SOUTH);
         frame.add(label, BorderLayout.NORTH);
@@ -49,18 +51,21 @@ public class ApplianceGUI implements ChangeListener {
 
     @Override
     public void stateChanged(ChangeEvent e) {
+        int value = slider.getValue();
 
-        if(slider.getValue() < 200) {
+        if (value < 200) {
             label.setText(("<html><div align='center'>" + title + "<br>(W) usage: <font color = 'blue'>" + slider.getValue() + "</div></html>"));
-        }
-        else if(slider.getValue() < 400) {
+        } else if (value < 400) {
             label.setText(("<html><div align='center'>" + title + "<br>(W) usage: <font color = 'green'>" + slider.getValue() + "</div></html>"));
-        }
-        else if(slider.getValue() < 700) {
+        } else if (value < 700) {
             label.setText(("<html><div align='center'>" + title + "<br>(W) usage: <font color = 'orange'>" + slider.getValue() + "</div></html>"));
-        }
-        else {
+        } else {
             label.setText(("<html><div align='center'>" + title + "<br>(W) usage: <font color = 'red'>" + slider.getValue() + "</div></html>"));
         }
+        buffer.put((double) value); // Sätter in värdet
+    }
+
+    public int getCurrentConsumption() { // hämtar värdet för appliance's användning
+        return slider.getValue();
     }
 }
