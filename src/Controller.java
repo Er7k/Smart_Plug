1b623332e9db492a4ceb0287e69fb7e31a566356
Er7k <Azyrite@gmail.com>
2025-03-06 10:35:20 +0100

    Implementerade kod så att man ser alla klienter samtidigt, fixade så att man alltid ser pekaren på gui

--- src/Controller.java
+++ src/Controller.java
@@ -9,6 +9,10 @@ public class Controller { // commit test
 
     private ServerGUI gui;
     private List<ApplianceGUI> clients;
+    final private int xLocationStart = 1030;
+    final private int xOffset = 192;
+    final private int yLocationStart = 0;
+    final private int yOffset = 500;
 
     public Controller() {
         gui = new ServerGUI("Server");
@@ -28,13 +32,13 @@ public class Controller { // commit test
 
     // alla klienter för appliances enligt tabellen
     private void initializeClients() {
-        clients.add(new ApplianceGUI("LED Light Bulb", 15));
-        clients.add(new ApplianceGUI("Incandescent Bulb", 40));
-        clients.add(new ApplianceGUI("Laptop", 30));
-        clients.add(new ApplianceGUI("Desktop Computer", 200));
-        clients.add(new ApplianceGUI("TV (LED)", 150));
-        clients.add(new ApplianceGUI("Refrigerator", 250));
-        clients.add(new ApplianceGUI("Microwave Oven", 1000));
+        clients.add(new ApplianceGUI("LED Light Bulb", 15, xLocationStart, yLocationStart));
+        clients.add(new ApplianceGUI("Incandescent Bulb", 40, xLocationStart + xOffset, yLocationStart));
+        clients.add(new ApplianceGUI("Laptop", 30, xLocationStart + (xOffset * 2), yLocationStart));
+        clients.add(new ApplianceGUI("Desktop Computer", 200, xLocationStart + (xOffset * 3), yLocationStart));
+        clients.add(new ApplianceGUI("TV (LED)", 150, xLocationStart, yOffset));
+        clients.add(new ApplianceGUI("Refrigerator", 250, xLocationStart + xOffset, yOffset));
+        clients.add(new ApplianceGUI("Microwave Oven", 1000, xLocationStart + (xOffset * 2), yOffset));
     }
 
     private void updateServerGUI() {
@@ -45,13 +49,26 @@ public class Controller { // commit test
             }
 
             final int finalTotalConsumption = totalConsumption; // Fick nån "lambda" error om jag inte gjorde detta
-            SwingUtilities.invokeLater(() -> {
-                gui.setTotalConsumption(finalTotalConsumption);
-            });
-            try {
-                Thread.sleep(1000); // 1 sekund frekvens för senaste consumption
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+            if(finalTotalConsumption <= 1000) {
+                SwingUtilities.invokeLater(() -> {
+                    gui.setTotalConsumption(finalTotalConsumption);
+                });
+                try {
+                    Thread.sleep(1000); // 1 sekund frekvens för senaste consumption
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                SwingUtilities.invokeLater(() -> {
+                    gui.setTotalConsumption(1000);
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
